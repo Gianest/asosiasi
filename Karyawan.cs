cf54a191e52e232747ade41e0238e2ab862086f1
dsgfresg <dsgdasf@>
2019-07-02 12:10:24 +0700

    Initial commit

---
+++ Karyawan.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asosiasi
+{
+    public class Karyawan
+    {
+        public string nama;
+        public bool status;
+        
+        public bool login(Kartu kartu)
+        {
+            return kartu.IsValid(this);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asosiasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kartu kartu = new Kartu();
+
+            Karyawan karyawan = new Karyawan();
+            karyawan.nama = "paijo";
+            karyawan.status = true;
+
+            if (karyawan.login(kartu)) {
+                Console.WriteLine("mashoook pak eko");
+            }
+            else {
+                Console.WriteLine("pakceklik");
+            }
+            Console.ReadKey();
+        }
+    
+    }
+}
